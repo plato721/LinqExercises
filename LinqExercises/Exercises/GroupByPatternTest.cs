9cbf3fcc3325a9af9ff6e61986a4680c4a04bcb5
Matt Ewell <matt.ewell@gmail.com>
2022-12-10 16:27:56 -0700

    Add GroupBy exercises

--- LinqExercises/Exercises/GroupByPatternTest.cs
+++ LinqExercises/Exercises/GroupByPatternTest.cs
@@ -1,25 +1,62 @@
-namespace LinqExercises.Solutions;
+using System.Collections;
 
-public class GroupByTest
+namespace LinqExercises.Exercises;
+
+public class GroupByPatternTest
 {
+  private record Group<TKey, TValue>(TKey Key) : IGrouping<TKey, TValue>
+  {
+    private readonly List<TValue> _values = new ();
+
+    public void Add(TValue value) => _values.Add(value);
+    public IEnumerator<TValue> GetEnumerator() => _values.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+  }
+  
   [Fact]
   public void GroupWordsByLength()
   {
-      var words = new List<string> { "sue", "alice", "steve", "sally", "adam", "fort", "tops", "dog", "cat" };
-      
-      var grouped = words.GroupBy(w => w.Length);
-
-      Assert.Equal(new List<string>{ "sue", "dog", "cat" }, grouped.First(g => g.Key == 3));
-      Assert.Equal(new List<string>{ "adam", "fort", "tops" }, grouped.First(g => g.Key == 4));
-      Assert.Equal(new List<string>{ "alice", "steve", "sally" }, grouped.First(g => g.Key == 5));
-  }
+    var words = new List<string> { "sue", "alice", "steve", "sally", "adam", "fort", "tops", "dog", "cat" };
+    var grouped = new List<Group<int, string>>();
     
-  [Fact]
+    foreach (var word in words)
+    {
+      if (grouped.Any(g => g.Key == word.Length))
+      {
+        grouped.First(g => g.Key == word.Length)
+          .Add(word);
+      }
+      else
+      {
+        var group = new Group<int, string>(word.Length);
+        group.Add(word);
+        grouped.Add(group);
+      }
+    }
+    
+    Assert.Equal(new List<string>{ "sue", "dog", "cat" }, grouped.First(g => g.Key == 3));
+    Assert.Equal(new List<string>{ "adam", "fort", "tops" }, grouped.First(g => g.Key == 4));
+    Assert.Equal(new List<string>{ "alice", "steve", "sally" }, grouped.First(g => g.Key == 5));
+  }
+
+  [Fact(Skip="Unskip when your code has been added")]
   public void GroupNumbersByOddAndEven()
   {
     var numbers = new List<int> { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+    var oddAndEven = new List<Group<int, int>>();
     
-    var oddAndEven = numbers.GroupBy(n => n % 2).ToList();
+    foreach (var number in numbers)
+    {
+      if (oddAndEven.Any(g => g.Key == number % 2))
+      {
+        // Your code here
+      }
+      else
+      {
+        // Your code here
+      }
+    }
 
     var expectedOdd = new List<int> { 1, 1, 3, 5, 13, 21, 55 };
     var expectedEven = new List<int> { 2, 8, 34 };
@@ -27,39 +64,42 @@ public class GroupByTest
     Assert.Equal(expectedOdd, oddAndEven.Find(g => g.Key == 1));
     Assert.Equal(expectedEven, oddAndEven.Find(g => g.Key == 0));
   }
-  
-  [Fact]
+
+  [Fact(Skip="Unskip when your code has been added")]
   public void GroupWordsByFirstLetter()
   {
     var words = new List<string> { "ant", "axis", "albatross", "bolt", "badge", "butter", "car", "cdr", "column" };
-    
-    var wordsByFirstLettter = words.GroupBy(w => w.First()).ToList();
-    
+    var wordsByFirstLettter = new List<Group<char, string>>();
+    foreach (var word in words)
+    {
+      // Your code here
+    }
     Assert.Equal(new List<string> { "ant", "axis", "albatross" }, wordsByFirstLettter.Find(g => g.Key == 'a'));
     Assert.Equal(new List<string> { "bolt", "badge", "butter" }, wordsByFirstLettter.Find(g => g.Key == 'b'));
     Assert.Equal(new List<string> { "car", "cdr", "column" }, wordsByFirstLettter.Find(g => g.Key == 'c'));
   }
-  
-  [Fact]
+
+  [Fact(Skip="Unskip when your code has been added")]
   public void GroupWordsByUniqueness()
   {
     var words = new List<string> { "one", "two", "one", "TWO", "three", "one", "three", "three", "three" };
+    var grouped = new List<Group<string, string>>();
     
-    var grouped = words.GroupBy(w => w.ToLower()).ToList();
-    
+    // Your code here
+
     Assert.Equal(new List<string> { "one", "one", "one" }, grouped.Find(g => g.Key == "one"));
     Assert.Equal(new List<string> { "two", "TWO" }, grouped.Find(g => g.Key == "two"));
     Assert.Equal(new List<string> { "three", "three", "three", "three" }, grouped.Find(g => g.Key == "three"));
   }
-  
-  [Fact]
+
+  [Fact(Skip="Unskip when your code has been added")]
   public void GroupByNumberOfZeros()
   {
     var numbers = new List<int> { 1, 3, 500, 200, 4000, 3000, 10000, 90, 20, 500000 };
-    
-    var grouped = numbers.GroupBy(n => Convert.ToString(n).Count(n => n == '0'))
-      .ToList();
-    
+    var grouped = new List<Group<int, int>>();
+
+    // Your code here
+
     Assert.Equal(new List<int> { 1, 3 }, grouped.Find(g => g.Key == 0));
     Assert.Equal(new List<int> { 90, 20 }, grouped.Find(g => g.Key == 1));
     Assert.Equal(new List<int> { 500, 200 }, grouped.Find(g => g.Key == 2));
@@ -67,15 +107,15 @@ public class GroupByTest
     Assert.Equal(new List<int> { 10000 }, grouped.Find(g => g.Key == 4));
     Assert.Equal(new List<int> { 500000 }, grouped.Find(g => g.Key == 5));
   }
-  
-  [Fact]
+
+  [Fact(Skip="Unskip when your code has been added")]
   public void GroupByOrderOfMagnitude()
   {
     var numbers = new List<int> { 1, 3, 503, 239, 4938, 3932, 19982, 93, 21, 501787 };
+    var grouped = new List<Group<int, int>>();
     
-    var grouped = numbers.GroupBy(n => Convert.ToString(n).Length)
-      .ToList();
-    
+    // Your code here
+
     Assert.Equal(new List<int> { 1, 3 }, grouped.Find(g => g.Key == 1));
     Assert.Equal(new List<int> { 93, 21 }, grouped.Find(g => g.Key == 2));
     Assert.Equal(new List<int> { 503, 239 }, grouped.Find(g => g.Key == 3));
--- LinqExercises/Exercises/GroupByTest.cs
+++ LinqExercises/Exercises/GroupByTest.cs
@@ -1,4 +1,4 @@
-namespace LinqExercises.Solutions;
+namespace LinqExercises.Exercises;
 
 public class GroupByTest
 {
@@ -14,12 +14,14 @@ public class GroupByTest
       Assert.Equal(new List<string>{ "alice", "steve", "sally" }, grouped.First(g => g.Key == 5));
   }
     
-  [Fact]
+  [Fact(Skip="Unskip when your code has been added")]
   public void GroupNumbersByOddAndEven()
   {
     var numbers = new List<int> { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
     
-    var oddAndEven = numbers.GroupBy(n => n % 2).ToList();
+    var oddAndEven = numbers.GroupBy(n =>
+      n // replace with your code .. can collapse into one line above
+    ).ToList();
 
     var expectedOdd = new List<int> { 1, 1, 3, 5, 13, 21, 55 };
     var expectedEven = new List<int> { 2, 8, 34 };
@@ -28,37 +30,42 @@ public class GroupByTest
     Assert.Equal(expectedEven, oddAndEven.Find(g => g.Key == 0));
   }
   
-  [Fact]
+  [Fact(Skip="Unskip when your code has been added")]
   public void GroupWordsByFirstLetter()
   {
     var words = new List<string> { "ant", "axis", "albatross", "bolt", "badge", "butter", "car", "cdr", "column" };
-    
-    var wordsByFirstLettter = words.GroupBy(w => w.First()).ToList();
+
+    var wordsByFirstLettter = words.GroupBy(w =>
+      'x' // replace with your code .. can collapse into one line above
+    ).ToList();
     
     Assert.Equal(new List<string> { "ant", "axis", "albatross" }, wordsByFirstLettter.Find(g => g.Key == 'a'));
     Assert.Equal(new List<string> { "bolt", "badge", "butter" }, wordsByFirstLettter.Find(g => g.Key == 'b'));
     Assert.Equal(new List<string> { "car", "cdr", "column" }, wordsByFirstLettter.Find(g => g.Key == 'c'));
   }
   
-  [Fact]
+  [Fact(Skip="Unskip when your code has been added")]
   public void GroupWordsByUniqueness()
   {
     var words = new List<string> { "one", "two", "one", "TWO", "three", "one", "three", "three", "three" };
     
-    var grouped = words.GroupBy(w => w.ToLower()).ToList();
+    var grouped = words.GroupBy(w =>
+      w // replace with your code .. can collapse into one line above
+    ).ToList();
     
     Assert.Equal(new List<string> { "one", "one", "one" }, grouped.Find(g => g.Key == "one"));
     Assert.Equal(new List<string> { "two", "TWO" }, grouped.Find(g => g.Key == "two"));
     Assert.Equal(new List<string> { "three", "three", "three", "three" }, grouped.Find(g => g.Key == "three"));
   }
   
-  [Fact]
+  [Fact(Skip="Unskip when your code has been added")]
   public void GroupByNumberOfZeros()
   {
     var numbers = new List<int> { 1, 3, 500, 200, 4000, 3000, 10000, 90, 20, 500000 };
     
-    var grouped = numbers.GroupBy(n => Convert.ToString(n).Count(n => n == '0'))
-      .ToList();
+    var grouped = numbers.GroupBy(n =>
+      0 // replace with your code .. can collapse into one line above
+    ).ToList();
     
     Assert.Equal(new List<int> { 1, 3 }, grouped.Find(g => g.Key == 0));
     Assert.Equal(new List<int> { 90, 20 }, grouped.Find(g => g.Key == 1));
@@ -68,13 +75,14 @@ public class GroupByTest
     Assert.Equal(new List<int> { 500000 }, grouped.Find(g => g.Key == 5));
   }
   
-  [Fact]
+  [Fact(Skip="Unskip when your code has been added")]
   public void GroupByOrderOfMagnitude()
   {
     var numbers = new List<int> { 1, 3, 503, 239, 4938, 3932, 19982, 93, 21, 501787 };
     
-    var grouped = numbers.GroupBy(n => Convert.ToString(n).Length)
-      .ToList();
+    var grouped = numbers.GroupBy(n =>
+        0 // replace with your code .. can collapse into one line above
+    ).ToList();
     
     Assert.Equal(new List<int> { 1, 3 }, grouped.Find(g => g.Key == 1));
     Assert.Equal(new List<int> { 93, 21 }, grouped.Find(g => g.Key == 2));
