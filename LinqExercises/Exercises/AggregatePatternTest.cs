85ef663e20ea52656c4a95645820b4ee10ed3b36
Matt Ewell <matt.ewell@gmail.com>
2022-12-29 14:41:57 -0700

    Add Aggregate

---
+++ LinqExercises/Exercises/AggregatePatternTest.cs
@@ -0,0 +1,94 @@
+namespace LinqExercises.Exercises;
+
+public class AggregatePatternTest
+{
+  [Fact]
+  public void SumAListOfNumbers()
+  {
+    var numbers = new List<int> { 32, 1, 21, 5, 81, 333 };
+    var result = 0;
+    foreach (var number in numbers)
+    {
+      result += number;
+    }
+    Assert.Equal(473, result);
+  }
+
+  [Fact]
+  public void SubtractListOfNumbers()
+  {
+    var numbers = new List<int> { 28, 12, 38, 1, 91 };
+    var result = 0;
+    foreach (var number in numbers)
+    {
+      // Your code here
+    }
+    Assert.Equal(-170, result);
+  }
+
+  [Fact]
+  public void MultiplyListOfNumber()
+  {
+    var numbers = new List<int> { 2, 3, 5, 7 };
+    var result = 1;
+    
+    // Your code here
+    
+    Assert.Equal(result, 210);
+  }
+
+  [Fact]
+  public void CapitalizeKeywordsInPhraseOneFishTwoFishRedFishBlueFish()
+  {
+    var keywords = new List<string> { "fish", "blue" };
+    var result = "one fish two fish red fish blue fish";
+
+    // Your code here
+
+    Assert.Equal("one FISH two FISH red FISH BLUE FISH", result);
+  }
+
+  [Fact]
+  public void Divide560ByABunchOfNumbers()
+  {
+    var numbers = new List<int>() { 2, 2, 2, 5, 7 };
+    var result = 560;
+    
+    // Your code here
+
+    Assert.Equal(2, result);
+  }
+
+  [Fact]
+  public void SubtractSmallestValuesFrom100()
+  {
+    var elements = new List<List<int>>
+    {
+      new() { 8, 5, 3 },
+      new() { 1, 9, 11 },
+      new() { 4, 7, 2 },
+      new() { 19, 34, 6 }
+    };
+    var result = 100;
+    
+    // Your code here
+
+    Assert.Equal(88, result);
+  }
+
+  [Fact]
+  public void AddAllTheSecondValuesTogether()
+  {
+    var elements = new List<List<object>>
+    {
+      new() { "a", 1 },
+      new() { "b", 9 },
+      new() { "c", 21 }
+    };
+    var result = 0;
+    
+    // Your code here
+
+    Assert.Equal(31, result);
+  }
+}
---
+++ LinqExercises/Exercises/AggregateTest.cs
@@ -0,0 +1,92 @@
+namespace LinqExercises.Solutions;
+
+public class AggregateTest
+{
+  [Fact]
+  public void SumAListOfNumbers()
+  {
+    var numbers = new List<int> { 32, 1, 21, 5, 81, 333 };
+    var result = numbers.Aggregate(0, (sum, number) =>
+    {
+      return sum + number;
+    });
+    Assert.Equal(473, result);
+  }
+
+  [Fact]
+  public void SubtractListOfNumbers()
+  {
+    var numbers = new List<int> { 28, 12, 38, 1, 91 };
+    var result = numbers.Aggregate(0, (difference, number) =>
+    {
+      return difference - number;
+    });
+    Assert.Equal(-170, result);
+  }
+
+  [Fact]
+  public void MultiplyListOfNumber()
+  {
+    var numbers = new List<int> { 2, 3, 5, 7 };
+    var result = numbers.Aggregate(1, (product, number) =>
+    {
+      return product * number;
+    });
+    Assert.Equal(result, 210);
+  }
+
+  [Fact]
+  public void CapitalizeKeywordsInPhraseOneFishTwoFishRedFishBlueFish()
+  {
+    var keywords = new List<string> { "fish", "blue" };
+    var result = keywords.Aggregate("one fish two fish red fish blue fish", (phrase, keyword) =>
+    {
+      return phrase.Replace(keyword, keyword.ToUpper());
+    });
+    Assert.Equal("one FISH two FISH red FISH BLUE FISH", result);
+  }
+
+  [Fact]
+  public void Divide560ByABunchOfNumbers()
+  {
+    var numbers = new List<int>() { 2, 2, 2, 5, 7 };
+    var result = numbers.Aggregate(560, (quotient, number) =>
+    {
+      return quotient / number;
+    });
+    Assert.Equal(2, result);
+  }
+
+  [Fact]
+  public void SubtractSmallestValuesFrom100()
+  {
+    var elements = new List<List<int>>
+    {
+      new() { 8, 5, 3 },
+      new() { 1, 9, 11 },
+      new() { 4, 7, 2 },
+      new() { 19, 34, 6 }
+    };
+    var result = elements.Aggregate(100, (difference, element) =>
+    {
+      return difference - element.Min();
+    });
+    Assert.Equal(88, result);
+  }
+
+  [Fact]
+  public void AddAllTheSecondValuesTogether()
+  {
+    var elements = new List<List<object>>
+    {
+      new() { "a", 1 },
+      new() { "b", 9 },
+      new() { "c", 21 }
+    };
+    var result = elements.Aggregate(0, (sum, element) =>
+    {
+      return sum + (int)element[1];
+    });
+    Assert.Equal(31, result);
+  }
+}
