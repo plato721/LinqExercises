0271408e4dd42e0369f9e3c071f6b3db8f2c9f6b
Matt Ewell <matt.ewell@gmail.com>
2023-01-25 21:21:48 -0700

    Add OrderBy exercises

---
+++ LinqExercises/Exercises/OrderByPatternTest.cs
@@ -0,0 +1,116 @@
+namespace LinqExercises.Exercises;
+
+public class OrderByPatternTest
+{
+  [Fact]
+  public void SortAlphabetically()
+  {
+    var words = new List<string> { "broccoli", "Carrots", "FISH", "Bacon", "candy" };
+
+    var transformed = new List<(string sortKey, string word)>();
+    foreach (var word in words)
+    {
+      transformed.Add((word.ToLower(), word));
+    }
+    transformed.Sort();
+
+    var sorted = new List<string>();
+    foreach (var (sortKey, thing) in transformed)
+    {
+      sorted.Add(thing);
+    }
+
+    var expected = new List<string> { "Bacon", "broccoli", "candy", "Carrots", "FISH" };
+    Assert.Equal(expected, sorted);
+  }
+
+  [Fact(Skip = "Remove this when your code is in place")]
+  public void SortAlphabeticallyByLastLetter()
+  {
+    var things = new List<string> { "pill", "box", "glass", "water", "sponge" };
+
+    var transformed = new List<(char sortKey, string thing)>();
+    foreach (var thing in things)
+    {
+      // Your code here
+    }
+    transformed.Sort();
+
+    var sorted = new List<string>();
+    foreach (var (sortKey,thing ) in transformed)
+    {
+      sorted.Add(thing);
+    }
+
+    var expected = new List<string> { "sponge", "pill", "water", "glass", "box" };
+    Assert.Equal(expected, sorted);
+  }
+
+  [Fact(Skip = "Remove this when your code is in place")]
+  public void SortByDistance()
+  {
+    var distances = new List<string> { "1cm", "9cm", "30cm", "4cm", "2cm" };
+
+    var transformed = new List<(int rawDistance, string distanceWithUnit)>();
+    foreach (var distance in distances)
+    {
+      // Your code here
+    }
+    transformed.Sort();
+
+    var sorted = new List<string>();
+    foreach (var (rawDistance, distance) in transformed)
+    {
+      // Your code here
+    }
+
+    var expected = new List<string> { "1cm", "2cm", "4cm", "9cm", "30cm" };
+    Assert.Equal(expected, sorted);
+  }
+
+  [Fact(Skip = "Remove this when your code is in place")]
+  public void SortByLength()
+  {
+    var words = new List<string> { "heteromorph", "ancyloceratina", "bioengineering", "mathematical", "bug" };
+
+    var transformed = new List<(int length, string word)>();
+    // Your code here
+    transformed.Sort();
+
+    var sorted = new List<string>();
+    // Your code here
+
+    var expected = new List<string> { "bug", "heteromorph", "mathematical", "ancyloceratina", "bioengineering" };
+    Assert.Equal(expected, sorted);
+  }
+
+  [Fact(Skip = "Remove this when your code is in place")]
+  public void SortByProximityToTen()
+  {
+    var prices = new List<double> { 3.02, 9.91, 17.9, 10.01, 11.0 };
+
+    var transformed = new List<(double proximityToTen, double number)>();
+    // Your code here
+
+    var sorted = new List<double>();
+    // Your code here
+
+    var expected = new List<double> { 10.01, 9.91, 11.0, 3.02, 17.9 };
+    Assert.Equal(expected, sorted);
+  }
+
+  [Fact(Skip = "Remove this when your code is in place")]
+  public void SortByNumberOfCents()
+  {
+    var prices = new List<double> { 3.02, 9.91, 7.9, 10.01, 11.0 };
+
+    // Your code here
+
+    var sorted = new List<double>();
+
+    // Your code here
+
+    var expected = new List<double> { 11.0, 10.01, 3.02, 7.9, 9.91 };
+    Assert.Equal(expected, sorted);
+  }
+}
